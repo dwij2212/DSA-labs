cd8ed7ad16c8331790f001491d7d8efbc8f7cf4e
Dwij Mehta <dwij.mehta@gmail.com>
2021-01-20 11:51:03 +0530

    Updated to take user inout

--- Lab1/seqListOps.c
+++ Lab1/seqListOps.c
@@ -25,73 +25,64 @@ void printJob(Job j)
 int inputJobs(JobList ele)
 {
     int i, size;
-    /*printf ("No of Jobs");
-  scanf ("%d",&size);
- for (i=0;i<10;i++)
- {
-  printf ("Enter job ID");
-  scanf ("%d",ele[i].id);
-  printf ("Enter Priority (from 0 - 2)");
-  scanf ("%d",ele[i].pri);
-  printf ("Execution Time");
-  scanf ("%d",ele[i].et);
-  printf ("Arrival Time");
-  scanf ("%d",ele[i].at);
- }*/
-    i = 0;
-    ele[0].id = 1;
-    ele[0].pri = 0;
-    ele[0].at = 1;
-    ele[0].et = 5;
-    ele[1].id = 2;
-    ele[1].pri = 1;
-    ele[1].at = 9;
-    ele[1].et = 3;
-    ele[2].id = 3;
-    ele[2].pri = 2;
-    ele[2].at = 11;
-    ele[2].et = 12;
-    ele[3].id = 4;
-    ele[3].pri = 0;
-    ele[3].at = 4;
-    ele[3].et = 7;
-    ele[4].id = 5;
-    ele[4].pri = 1;
-    ele[4].at = 8;
-    ele[4].et = 2;
-    ele[5].id = 6;
-    ele[5].pri = 2;
-    ele[5].at = 12;
-    ele[5].et = 11;
-    ele[6].id = 7;
-    ele[6].pri = 0;
-    ele[6].at = 3;
-    ele[6].et = 9;
-    ele[7].id = 8;
-    ele[7].pri = 1;
-    ele[7].at = 7;
-    ele[7].et = 4;
-    ele[8].id = 9;
-    ele[8].pri = 2;
-    ele[8].at = 13;
-    ele[8].et = 10;
-    ele[9].id = 10;
-    ele[9].pri = 0;
-    ele[9].at = 2;
-    ele[9].et = 15;
-    return 10;
-    /* ele[0].pri = ele[3].pri = ele[6].pri = PRI_0;
- ele[1].pri = ele[4].pri = ele[7].pri = PRI_1;
- ele[2].pri = ele[5].pri = ele[8].pri = ele[9].pri = PRI_2;
- for (i=0;i<MAX;i++)
- {
-  ele[i].id = i+1;
-  ele[i].et = i+2;
-  ele[i].at = i;
- }
- ele[2].at = 10;
- ele[5].at = 1;
- ele[6].at = 5;*/
+    printf("No of Jobs");
+    scanf("%d", &size);
+    for (i = 0; i < 10; i++)
+    {
+        printf("Enter job ID");
+        scanf("%d", ele[i].id);
+        printf("Enter Priority (from 0 - 2)");
+        scanf("%d", ele[i].pri);
+        printf("Execution Time");
+        scanf("%d", ele[i].et);
+        printf("Arrival Time");
+        scanf("%d", ele[i].at);
+    }
+
+    return size;
+
+    // Dummy data
+    // ele[0].id = 1;
+    // ele[0].pri = 0;
+    // ele[0].at = 1;
+    // ele[0].et = 5;
+    // ele[1].id = 2;
+    // ele[1].pri = 1;
+    // ele[1].at = 9;
+    // ele[1].et = 3;
+    // ele[2].id = 3;
+    // ele[2].pri = 2;
+    // ele[2].at = 11;
+    // ele[2].et = 12;
+    // ele[3].id = 4;
+    // ele[3].pri = 0;
+    // ele[3].at = 4;
+    // ele[3].et = 7;
+    // ele[4].id = 5;
+    // ele[4].pri = 1;
+    // ele[4].at = 8;
+    // ele[4].et = 2;
+    // ele[5].id = 6;
+    // ele[5].pri = 2;
+    // ele[5].at = 12;
+    // ele[5].et = 11;
+    // ele[6].id = 7;
+    // ele[6].pri = 0;
+    // ele[6].at = 3;
+    // ele[6].et = 9;
+    // ele[7].id = 8;
+    // ele[7].pri = 1;
+    // ele[7].at = 7;
+    // ele[7].et = 4;
+    // ele[8].id = 9;
+    // ele[8].pri = 2;
+    // ele[8].at = 13;
+    // ele[8].et = 10;
+    // ele[9].id = 10;
+    // ele[9].pri = 0;
+    // ele[9].at = 2;
+    // ele[9].et = 15;
+    // return 10;
 }
 
 SeqList insert(Job j, SeqList sl)
